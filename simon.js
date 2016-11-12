977c939f9eca65975777e59496a949a8acd5d543
John Clark <johngclark36@gmail.com>
2016-11-12 17:57:38 -0500

    Add #handleMistake function which plays a mistake sound effect and either restarts the pattern or resetes it based on whether or not strictMode is on

--- simon.js
+++ simon.js
@@ -6,7 +6,18 @@ $( document ).ready(function () {
 
 
 // GAME LOGIC
+  var strictMode = false;
   var pattern = [];
+  var userInput = [];
+
+  $("div").on("click",function (e) {
+    var $clicked = $(this);
+    var num = parseInt($clicked.text());
+    highlightElement(num, removeHighlight)
+    var audio = new Audio(audios[num - 1]);
+    audio.play();
+    userInput.push(num);
+  });
 
   var generatePattern = function (arr) {
     var num = Math.floor(Math.random() * 4) + 1;
@@ -36,17 +47,6 @@ $( document ).ready(function () {
   }
   showPattern();
 
-  var userInput = [];
-  $("div").on("click",function (e) {
-    var $clicked = $(this);
-    var num = parseInt($clicked.text());
-    highlightElement(num, removeHighlight)
-    var audio = new Audio(audios[num - 1]);
-    audio.play();
-    userInput.push(num);
-  });
-
-
   function checkUserInput (input, pattern, cb) {
     var idx = 0;
     var wait = 0;
@@ -58,7 +58,7 @@ $( document ).ready(function () {
           clearInterval(checkId);
           console.log("correct");
           userInput = [];
-          if (pattern.length === 2) { victory(); } else {
+          if (pattern.length === 20) { victory(); } else {
             generatePattern(pattern);
             cb();
           }
@@ -66,10 +66,10 @@ $( document ).ready(function () {
         }
         else if (pattern[idx] === input[idx]) {
           idx += 1;
-          console.log(input);
+          console.log(input)
         } else {
             clearInterval(checkId);
-            console.log("mistake");
+            handleMistake();
         }
       } else {
         if (wait < pattern.length + 1) {
@@ -87,6 +87,22 @@ $( document ).ready(function () {
 
   }
 
+  function handleMistake() {
+    let audio = new Audio('./audio/mistake.mp3')
+    audio.play()
+    setTimeout(()=> {
+      if (strictMode) {
+        pattern = []
+        userInput = [];
+        generatePattern(pattern)
+        showPattern()
+      } else {
+        userInput = []
+        showPattern()
+      }
+    }, 5 * 1000)
+  }
+
   function victory() {
     let audio = new Audio('./audio/victory.mp3')
     audio.play();
@@ -95,9 +111,7 @@ $( document ).ready(function () {
       generatePattern(pattern)
       showPattern()
     }, 14 * 1000)
-
   }
-
 });
 ////////////////////////////////////////////////
 
@@ -114,13 +128,3 @@ function highlightElement (id, cb) {
 function removeHighlight(id) {
   $("#" + id).removeClass("highlight")
 }
-
-
-
-
-// 1.  generate pattern
-//
-// 2.  show pattern
-//
-// 3.  get user input and check if input matches the current pattern
-//   a)  if so, repeat steps
